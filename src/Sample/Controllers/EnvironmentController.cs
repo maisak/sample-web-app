c203ec90239aa56827f5f1e9b8aefdb07eba8101
Anthony <anthony.maisak@hotmail.com>
2020-02-15 16:50:15 +0200

    Environment page done.

---
+++ src/Sample/Controllers/EnvironmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Common.Toolbox;
+
+namespace Sample.Controllers
+{
+    public class EnvironmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["InstanceId"] = EnvironmentConfig.InstanceId;
+            ViewData["IsStaging"] = EnvironmentConfig.IsStaging;
+            ViewData["Settable"] = EnvironmentConfig.Settable;
+
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string value)
+        {
+            EnvironmentConfig.Settable = value;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
